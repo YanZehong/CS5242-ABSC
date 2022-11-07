bf688465ba760aa7e6ffc0af364a2c9c8c35d00a
YanZehong <yanzehong1101@outlook.com>
2022-11-07 00:44:31 +0000

    update Transformer and BERT

--- data_utils.py
+++ data_utils.py
@@ -6,8 +6,11 @@ import pandas as pd
 import math
 import collections
 import nltk
+from nltk.corpus import stopwords
 # nltk.download('punkt')
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Tokenizer(object):
     """Construct a tokenizer."""
@@ -190,3 +193,81 @@ def parse_dataset(data_dir, mode):
     print(f"ratio: [{label_cnt[0]/sum(label_cnt)*100 :.1f}%, {label_cnt[1]/sum(label_cnt)*100 :.1f}%, {label_cnt[2]/sum(label_cnt)*100 :.1f}%]")
     print(f"#examples: {len(corpus)}")
     return corpus
+
+def plot_ngram(df, aspect, k):
+    # Splitting the corpus into positive and negative comments
+    positive_comments = df[df[aspect] == 2]['review']
+    negative_comments = df[df[aspect] == 1]['review']
+    neutral_comments = df[df[aspect] == 0]['review']
+    # Extracting the top k unigrams by sentiment
+    trigrams_pos = ngrams_count(positive_comments, (3, 3), k)
+    trigrams_neg = ngrams_count(negative_comments, (3, 3), k)
+    trigrams_neu = ngrams_count(neutral_comments, (3, 3), k)
+
+    # Joining everything in a python dictionary to make the plots easier
+    ngram_dict_plot = {
+        'Top Trigrams on Positive Comments': trigrams_pos,
+        'Top Trigrams on Negative Comments': trigrams_neg,
+        'Top Trigrams on Neutral Comments': trigrams_neu,
+    }
+
+    # Plotting the ngrams analysis
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 18))
+    i, j = 0, 0
+    colors = ['Blues_d', 'Reds_d', 'Greens_d']
+    for title, ngram_data in ngram_dict_plot.items():
+        # ax = axs[i, j]
+        ax = axs[j]
+        sns.barplot(x='count', y='ngram', data=ngram_data, ax=ax, palette=colors[j])
+        
+        # Customizing plots
+        format_spines(ax, right_border=False)
+        ax.set_title(title, size=14)
+        ax.set_ylabel('')
+        ax.set_xlabel('')
+        
+        # Incrementing the index
+        j += 1
+        # if j == 3:
+        #     j = 0
+        #     i += 1
+    plt.tight_layout()
+    plt.show()
+
+def format_spines(ax, right_border=True):
+    """docstring for format_spines:
+    this function sets up borders from an axis and personalize colors
+    input:
+        ax: figure axis
+        right_border: flag to determine if the right border will be visible or not"""
+    
+    # Setting up colors
+    ax.spines['bottom'].set_color('#CCCCCC')
+    ax.spines['left'].set_color('#CCCCCC')
+    ax.spines['top'].set_color('#FFFFFF')
+    if right_border:
+        ax.spines['right'].set_color('#CCCCCC')
+    else:
+        ax.spines['right'].set_color('#FFFFFF')
+    ax.patch.set_facecolor('#FFFFFF')
+
+def ngrams_count(corpus, ngram_range, n=-1, cached_stopwords=stopwords.words('portuguese')):
+    """
+    Args
+    ----------
+    corpus: text to be analysed [type: pd.DataFrame]
+    ngram_range: type of n gram to be used on analysis [type: tuple]
+    n: top limit of ngrams to be shown [type: int, default: -1]
+    """
+    
+    # Using CountVectorizer to build a bag of words using the given corpus
+    vectorizer = CountVectorizer(stop_words=cached_stopwords, ngram_range=ngram_range).fit(corpus)
+    bag_of_words = vectorizer.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0)
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vectorizer.vocabulary_.items()]
+    words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+    total_list = words_freq[:n]
+    
+    # Returning a DataFrame with the ngrams count
+    count_df = pd.DataFrame(total_list, columns=['ngram', 'count'])
+    return count_df
